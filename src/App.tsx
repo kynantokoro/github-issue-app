6228d7de81acb393ed18f6d7e4af9ee65352634d
kynantokoro <tokoro@kynantokoro.com>
2021-02-09 17:32:59 +0900

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
